8c8b7fa038027695974e5654666e9cd464167974
wrt <wrt@bt.foo.org>
2014-05-06 13:16:49 +0800

    添加7620 wifi

---
+++ target/linux/ramips/files/drivers/mtd/ralink/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/mtd/ralink/built-in.o :=  mipsel-openwrt-linux-uclibc-ld  -m elf32ltsmip   -r -o drivers/mtd/ralink/built-in.o drivers/mtd/ralink/ralink-flash.o 
---
+++ target/linux/ramips/files/drivers/net/wireless/rt2860v2_ap/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/net/wireless/rt2860v2_ap/built-in.o :=  mipsel-openwrt-linux-uclibc-ld  -m elf32ltsmip   -r -o drivers/net/wireless/rt2860v2_ap/built-in.o drivers/net/wireless/rt2860v2_ap/rt2860v2_ap.o 
